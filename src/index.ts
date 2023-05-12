28bf74f46e8edecf8ecd72fb655d618045f0bc94
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2023-05-12 12:16:01 -0300

    Bring in relevant `src/` changes from `target` branch. (#7)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from './forkTests';
 export * from './signers';
 export * from './task';
 export * from './test';
+export * from './types';
 export * from './network';
+export * from './contracts';
 
 export { default as Task } from './task';
