043d71014ac86971a528797da9524b9484b36445
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2023-07-12 15:35:11 -0300

    use usdc instead of bal

--- tasks/20230711-composable-stable-pool-v5/input.ts
+++ tasks/20230711-composable-stable-pool-v5/input.ts
@@ -12,7 +12,7 @@ export type ComposableStablePoolDeployment = {
 const Vault = new Task('20210418-vault', TaskMode.READ_ONLY);
 const ProtocolFeePercentagesProvider = new Task('20220725-protocol-fee-percentages-provider', TaskMode.READ_ONLY);
 const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
-const BAL = new Task('00000000-tokens', TaskMode.READ_ONLY);
+const USDC = new Task('00000000-tokens', TaskMode.READ_ONLY);
 
 const BaseVersion = { version: 5, deployment: '20230711-composable-stable-pool-v5' };
 
@@ -20,7 +20,7 @@ export default {
   Vault,
   ProtocolFeePercentagesProvider,
   WETH,
-  BAL,
+  USDC,
   FactoryVersion: JSON.stringify({ name: 'ComposableStablePoolFactory', ...BaseVersion }),
   PoolVersion: JSON.stringify({ name: 'ComposableStablePool', ...BaseVersion }),
 };
