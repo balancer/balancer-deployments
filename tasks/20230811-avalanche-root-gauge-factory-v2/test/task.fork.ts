22c7f941f49dfc3ed02b8e6ba289065514ebf21f
EndymionJkb <EndymionJkb@gmail.com>
2023-09-05 18:11:21 -0400

    refactor: restore root gauge code to the monorepo (#82)

--- tasks/20230811-avalanche-root-gauge-factory-v2/test/task.fork.ts
+++ tasks/20230811-avalanche-root-gauge-factory-v2/test/task.fork.ts
@@ -337,7 +337,7 @@ describeForkTest('AvalancheRootGaugeFactory V2', 'mainnet', 17879200, function (
     sharedBeforeEach(async () => {
       const input = task.input() as AvalancheRootGaugeFactoryDeployment;
       // We need to set force to `true`.
-      // Source code for this Mock is in src/helpers/contracts
+      // The mock root gauge code is in the monorepo, in liquidity-mining/contracts/test.
       mockGauge = await task.deploy('MockAvalancheRootGauge', [input.BalancerMinter, input.BALProxy], admin, true);
 
       // Fund mock gauge with BAL
