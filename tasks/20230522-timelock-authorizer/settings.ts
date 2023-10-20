01f34b44f64d4f18d0bc6b92f3f49b19e7916996
Juan Ignacio Ubeira <juani@balancer.finance>
2023-10-20 15:40:13 -0300

    Address comments.

--- tasks/20230522-timelock-authorizer/settings.ts
+++ tasks/20230522-timelock-authorizer/settings.ts
@@ -1,8 +1,15 @@
-/// This file represents configuration settings used by the contract after its deployment.
-/// The original deployment input settings must be kept intact to be able to verify the deployments.
-const settings = {
-  goerli: require('./settings/goerli.ts'),
-  sepolia: require('./settings/sepolia.ts'),
-};
+import input from './input';
+
+// This file represents configuration settings used by the contract after its deployment.
+// The original deployment input settings must be kept intact to be able to verify the deployments.
+
+const networks = input.networks;
+
+// Assign settings file for each deployed network.
+const settings = Object.fromEntries(
+  networks.map((network: string) => {
+    return [network, require(`./settings/${network}.ts`)];
+  })
+);
 
 export default settings;
