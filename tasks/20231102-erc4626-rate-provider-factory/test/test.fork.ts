b4781a27d186223ab754322fac2ceaa5f60713b4
baileyspraggins <baileyspraggins@me.com>
2023-11-02 14:33:38 -0500

    Adjusts tests to use an updated block number

--- tasks/20231102-erc4626-rate-provider-factory/test/test.fork.ts
+++ tasks/20231102-erc4626-rate-provider-factory/test/test.fork.ts
@@ -6,21 +6,23 @@ import * as expectEvent from '@helpers/expectEvent';
 import { ZERO_ADDRESS } from '@helpers/constants';
 import { fp } from '@helpers/numbers';
 
-describeForkTest('ERC4626RateProviderFactory', 'mainnet', 17717232, function () {
+describeForkTest('ERC4626RateProviderFactory', 'mainnet', 18380714, function () {
   let task: Task;
   let erc4626: Contract, rateProviderFactory: Contract, rateProvider: Contract;
-
+  
   let sFraxErc4626Address = '0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32';
+
   before('run task', async () => {
     task = new Task('20231102-erc4626-rate-provider-factory', TaskMode.TEST, getForkedNetwork(hre));
     await task.run({ force: true });
 
     erc4626 = await task.instanceAt('IERC4626', sFraxErc4626Address);
+
     rateProviderFactory = await task.deployedInstance('ERC4626RateProviderFactory');
   });
 
   before('create a ERC4626RateProvider', async () => {
-    const receipt = await (await rateProviderFactory.create(erc4626)).wait();
+    const receipt = await (await rateProviderFactory.create(erc4626.address)).wait();
     const event = await expectEvent.inReceipt(receipt, 'RateProviderCreated');
 
     rateProvider = await task.instanceAt('ERC4626RateProvider', event.args.rateProvider);
