0e788084b5ad59ea7415fcadf972ca3737253cfc
Juan Ignacio Ubeira <juani@balancer.finance>
2024-06-19 12:51:51 -0300

    Deploy veBoost V2 to frax.

--- tasks/20230525-l2-veboost-v2/input.ts
+++ tasks/20230525-l2-veboost-v2/input.ts
@@ -1,3 +1,5 @@
+import Task, { TaskMode } from 'task';
+
 export type L2VeBoostV2Deployment = {
   VotingEscrow: string;
 };
@@ -5,30 +7,50 @@ export type L2VeBoostV2Deployment = {
 export default {
   // https://polygonscan.com/address/0xE241C6e48CA045C7f631600a0f1403b2bFea05ad
   polygon: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'polygon',
+    }).OmniVotingEscrowChild,
   },
   // https://arbiscan.io/address/0xe241c6e48ca045c7f631600a0f1403b2bfea05ad
   arbitrum: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'arbitrum',
+    }).OmniVotingEscrowChild,
   },
   // https://optimistic.etherscan.io/address/0xe241c6e48ca045c7f631600a0f1403b2bfea05ad
   optimism: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'optimism',
+    }).OmniVotingEscrowChild,
   },
   // https://gnosisscan.io/address/0xe241c6e48ca045c7f631600a0f1403b2bfea05ad
   gnosis: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'gnosis',
+    }).OmniVotingEscrowChild,
   },
   // https://snowtrace.io/address/0xe241c6e48ca045c7f631600a0f1403b2bfea05ad
   avalanche: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'avalanche',
+    }).OmniVotingEscrowChild,
   },
   // https://zkevm.polygonscan.com/address/0xe241c6e48ca045c7f631600a0f1403b2bfea05ad
   zkevm: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'zkevm',
+    }).OmniVotingEscrowChild,
   },
   // https://basescan.org/address/0xE241C6e48CA045C7f631600a0f1403b2bFea05ad
   base: {
-    VotingEscrow: '0xE241C6e48CA045C7f631600a0f1403b2bFea05ad',
+    VotingEscrow: new Task('20230524-base-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'base',
+    }).OmniVotingEscrowChild,
+  },
+  // https://basescan.org/address/0xE241C6e48CA045C7f631600a0f1403b2bFea05ad
+  fraxtal: {
+    VotingEscrow: new Task('20230524-lz-omni-voting-escrow-child', TaskMode.READ_ONLY).output({
+      network: 'fraxtal',
+    }).OmniVotingEscrowChild,
   },
 };
