8cfa24dae566f791d335d81e469471c6f9e04cb9
Juan Ignacio Ubeira <juani@balancer.finance>
2024-12-04 18:49:55 -0300

    Fix task reference.

--- v3/tasks/20241204-v3-vault/input.ts
+++ v3/tasks/20241204-v3-vault/input.ts
@@ -15,7 +15,7 @@ export type VaultDeployment = {
 };
 
 const Authorizer = new Task('20210418-authorizer', TaskMode.READ_ONLY);
-const Vault = new Task('v3-vault-factory', TaskMode.READ_ONLY);
+const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 
 const vaultArtifact = Vault.artifact('Vault');
 const vaultCreationCode = vaultArtifact.bytecode;
