24efaa16b9d188ef2d3bb605f0de2d89fa3e01db
João Bruno <joaobrunoah@gmail.com>
2024-12-05 15:59:36 -0300

    Fix tests

--- v3/tasks/20241205-v3-composite-liquidity-router/input.ts
+++ v3/tasks/20241205-v3-composite-liquidity-router/input.ts
@@ -13,7 +13,7 @@ export type RouterDeployment = {
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
 const Permit2 = new Task('00000000-permit2', TaskMode.READ_ONLY);
-const BaseVersion = { version: 1, deployment: '20241205-v3-routers' };
+const BaseVersion = { version: 1, deployment: '20241205-v3-router' };
 
 export default {
   Vault,
