591373b91c1b10ddf64f1268a4383dd3bd1910c5
João Bruno <joaobrunoah@gmail.com>
2024-12-05 16:06:36 -0300

    Fix CompositeLiquidityRouter

--- v3/tasks/20241205-v3-composite-liquidity-router/input.ts
+++ v3/tasks/20241205-v3-composite-liquidity-router/input.ts
@@ -13,7 +13,7 @@ export type RouterDeployment = {
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
 const Permit2 = new Task('00000000-permit2', TaskMode.READ_ONLY);
-const BaseVersion = { version: 1, deployment: '20241205-v3-router' };
+const BaseVersion = { version: 1, deployment: '20241205-v3-composite-liquidity-router' };
 
 export default {
   Vault,
--- v3/tasks/20241205-v3-composite-liquidity-router/test/task.fork.ts
+++ v3/tasks/20241205-v3-composite-liquidity-router/test/task.fork.ts
@@ -13,35 +13,18 @@ describeForkTest('CompositeLiquidityRouter-V3', 'mainnet', 21336200, function ()
   const WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
   const versionNumber = 1;
-  const deploymentId = '20241205-v3-routers';
+  const deploymentId = '20241205-v3-composite-liquidity-router';
 
   before('run task', async () => {
     task = new Task(deploymentId, TaskMode.TEST, getForkedNetwork(hre));
     await task.run({ force: true });
 
-    router = await task.deployedInstance('Router');
-    batchRouter = await task.deployedInstance('BatchRouter');
     compositeLiquidityRouter = await task.deployedInstance('CompositeLiquidityRouter');
-    bufferRouter = await task.deployedInstance('BufferRouter');
 
     const testBALTokenTask = new Task('20220325-test-balancer-token', TaskMode.READ_ONLY, getForkedNetwork(hre));
     const WETH = await testBALTokenTask.instanceAt('TestBalancerToken', WETH_ADDRESS);
     wethSigner = await impersonate(WETH.address, fp(10e8));
     alice = await getSigner();
-  });
-
-  it('checks router version', async () => {
-    const routerVersion = JSON.parse(await router.version());
-    expect(routerVersion.name).to.be.eq('Router');
-    expect(routerVersion.version).to.be.eq(versionNumber);
-    expect(routerVersion.deployment).to.be.eq(deploymentId);
-  });
-
-  it('checks batch router version', async () => {
-    const batchRouterVersion = JSON.parse(await batchRouter.version());
-    expect(batchRouterVersion.name).to.be.eq('BatchRouter');
-    expect(batchRouterVersion.version).to.be.eq(versionNumber);
-    expect(batchRouterVersion.deployment).to.be.eq(deploymentId);
   });
 
   it('checks composite liquidity router version', async () => {
@@ -51,22 +34,15 @@ describeForkTest('CompositeLiquidityRouter-V3', 'mainnet', 21336200, function ()
     expect(compositeLiquidityRouterVersion.deployment).to.be.eq(deploymentId);
   });
 
-  it('checks buffer router version', async () => {
-    const bufferRouterVersion = JSON.parse(await bufferRouter.version());
-    expect(bufferRouterVersion.name).to.be.eq('BufferRouter');
-    expect(bufferRouterVersion.version).to.be.eq(versionNumber);
-    expect(bufferRouterVersion.deployment).to.be.eq(deploymentId);
-  });
-
-  it('checks router WETH', async () => {
+  it('checks composite liquidity router WETH', async () => {
     const wethTx = wethSigner.sendTransaction({
-      to: router.address,
+      to: compositeLiquidityRouter.address,
       value: ethers.utils.parseEther('1.0'),
     });
     await expect(wethTx).to.not.be.reverted;
 
     const aliceTx = alice.sendTransaction({
-      to: router.address,
+      to: compositeLiquidityRouter.address,
       value: ethers.utils.parseEther('1.0'),
     });
     await expect(aliceTx).to.be.reverted;
