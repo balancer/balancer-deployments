40ab093b3cec86ed395137420d2237724b2ee5c0
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2025-02-13 11:02:07 -0300

    Deployment preparation: `MevCaptureHook` (#174)

---
+++ v3/tasks/20250212-v3-mev-capture-hook/input.ts
@@ -0,0 +1,14 @@
+import { Task, TaskMode } from '@src';
+
+export type MevCaptureHookDeployment = {
+  Vault: string;
+  BalancerContractRegistry: string;
+};
+
+const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
+const BalancerContractRegistry = new Task('20250117-v3-contract-registry', TaskMode.READ_ONLY);
+
+export default {
+  Vault,
+  BalancerContractRegistry,
+};
