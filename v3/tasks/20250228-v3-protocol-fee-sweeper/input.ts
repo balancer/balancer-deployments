c65f31d5ed86fbafc8952b4ab689fa2e519423fa
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2025-02-27 14:06:35 -0300

    Update v3/tasks/20250228-v3-protocol-fee-sweeper/input.ts

--- v3/tasks/20250228-v3-protocol-fee-sweeper/input.ts
+++ v3/tasks/20250228-v3-protocol-fee-sweeper/input.ts
@@ -11,24 +11,20 @@ const MaxiOmniMultisig = '0x9ff471F9f98F42E5151C7855fD1b5aa906b1AF7e';
 const SepoliaAdmin = '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673';
 
 export default {
+  Vault,
   arbitrum: {
-    Vault,
     FeeRecipient: MaxiOmniMultisig,
   },
   base: {
-    Vault,
     FeeRecipient: MaxiOmniMultisig,
   },
   gnosis: {
-    Vault,
     FeeRecipient: MaxiOmniMultisig,
   },
   mainnet: {
-    Vault,
     FeeRecipient: MaxiOmniMultisig,
   },
   sepolia: {
-    Vault,
     FeeRecipient: SepoliaAdmin,
   },
 };
