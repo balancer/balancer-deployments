ae4bc364abd8118a444b5b854ece919194f6dcbc
Jeff Bennett <endymionjkb@gmail.com>
2025-02-27 11:28:24 -0500

    generalize FeeRecipient for Sepolia

--- v3/tasks/20250228-v3-protocol-fee-sweeper/input.ts
+++ v3/tasks/20250228-v3-protocol-fee-sweeper/input.ts
@@ -7,9 +7,28 @@ export type ProtocolFeeSweeperDeployment = {
 
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 // See https://github.com/BalancerMaxis/bal_addresses/blob/main/outputs/addressbook.json#L268
-const FeeRecipient = '0x9ff471F9f98F42E5151C7855fD1b5aa906b1AF7e';
+const MaxiOmniMultisig = '0x9ff471F9f98F42E5151C7855fD1b5aa906b1AF7e';
+const SepoliaAdmin = '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673';
 
 export default {
-  Vault,
-  FeeRecipient,
+  arbitrum: {
+    Vault,
+    FeeRecipient: MaxiOmniMultisig,
+  },
+  base: {
+    Vault,
+    FeeRecipient: MaxiOmniMultisig,
+  },
+  gnosis: {
+    Vault,
+    FeeRecipient: MaxiOmniMultisig,
+  },
+  mainnet: {
+    Vault,
+    FeeRecipient: MaxiOmniMultisig,
+  },
+  sepolia: {
+    Vault,
+    FeeRecipient: SepoliaAdmin,
+  },
 };
