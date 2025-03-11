57cc7a02db13cf6c1586533269ec6882d84a2dda
Jeff Bennett <endymionjkb@gmail.com>
2025-03-10 23:23:15 -0400

    remove temporary code

--- v3/scripts/20250221-protocol-fee-controller-migration/index.ts
+++ v3/scripts/20250221-protocol-fee-controller-migration/index.ts
@@ -4,8 +4,7 @@ import { ProtocolFeeControllerMigrationDeployment } from './input';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as ProtocolFeeControllerMigrationDeployment;
 
-  //const args = [input.Vault, input.ProtocolFeeController];
-  const args = [input.Vault];
+  const args = [input.Vault, input.ProtocolFeeController];
 
   await task.deployAndVerify('ProtocolFeeControllerMigration', args, from, force);
 
--- v3/scripts/20250221-protocol-fee-controller-migration/input.ts
+++ v3/scripts/20250221-protocol-fee-controller-migration/input.ts
@@ -2,13 +2,13 @@ import { Task, TaskMode } from '@src';
 
 export type ProtocolFeeControllerMigrationDeployment = {
   Vault: string;
-  // TEMPORARY! ProtocolFeeController: string;
+  ProtocolFeeController: string;
 };
 
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
-//const ProtocolFeeController = new Task('20250214-v3-protocol-fee-controller-v2', TaskMode.READ_ONLY);
+const ProtocolFeeController = new Task('20250214-v3-protocol-fee-controller-v2', TaskMode.READ_ONLY);
 
 export default {
   Vault,
-  //ProtocolFeeController,
+  ProtocolFeeController,
 };
