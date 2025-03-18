9800d4d7a7380247a20bb6fbd6756bab4def156d
EndymionJkb <EndymionJkb@gmail.com>
2025-03-18 10:56:36 -0400

    Deprecate Router V1 (#210)
    * Move original router to deprecated folder
    
    * docs: update changelog and readme - also correct Cow Swap Fee Burner error
    
    * restore artifact
    
    * update addresses

--- v3/deprecated/20241205-v3-composite-liquidity-router/test/task.fork.ts
+++ v3/deprecated/20241205-v3-composite-liquidity-router/test/task.fork.ts
@@ -5,7 +5,7 @@ import { Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { fp } from '@helpers/numbers';
 
-describeForkTest.skip('CompositeLiquidityRouter-V3', 'mainnet', 21336200, function () {
+describeForkTest('CompositeLiquidityRouter-V3', 'mainnet', 21336200, function () {
   let task: Task;
   let compositeLiquidityRouter: Contract;
   let wethSigner: SignerWithAddress, alice: SignerWithAddress;
