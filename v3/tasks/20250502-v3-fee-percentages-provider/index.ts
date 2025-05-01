527c5db72b994a54489b634e127f4304085f85c1
Jeff Bennett <endymionjkb@gmail.com>
2025-05-01 12:54:40 -0400

    Construct without explicit fee controller

--- v3/tasks/20250502-v3-fee-percentages-provider/index.ts
+++ v3/tasks/20250502-v3-fee-percentages-provider/index.ts
@@ -4,7 +4,7 @@ import { ProtocolFeePercentagesProviderDeployment } from './input';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as ProtocolFeePercentagesProviderDeployment;
 
-  const args = [input.Vault, input.ProtocolFeeController, input.BalancerContractRegistry];
+  const args = [input.Vault, input.BalancerContractRegistry];
 
   await task.deployAndVerify('ProtocolFeePercentagesProvider', args, from, force);
 };
