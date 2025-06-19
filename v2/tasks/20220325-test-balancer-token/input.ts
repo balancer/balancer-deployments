6e057dd50f499d97951a347ec456a2d2f0215bd6
Juan Ignacio Ubeira <juani@balancer.finance>
2025-06-19 15:02:23 -0300

    Test token deployment.

--- v2/tasks/20220325-test-balancer-token/input.ts
+++ v2/tasks/20220325-test-balancer-token/input.ts
@@ -3,13 +3,28 @@ export type TestBalancerTokenDeployment = {
 };
 
 export default {
-  sepolia: {
-    Admin: '0x171C0fF5943CE5f133130436A29bF61E26516003',
+  arbitrum: {
+    Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
+  },
+  avalanche: {
+    Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
   },
   base: {
     Admin: '0xC40DCFB13651e64C8551007aa57F9260827B6462',
   },
+  gnosis: {
+    Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
+  },
   hyperevm: {
     Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
+  },
+  mainnet: {
+    Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
+  },
+  optimism: {
+    Admin: '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673',
+  },
+  sepolia: {
+    Admin: '0x171C0fF5943CE5f133130436A29bF61E26516003',
   },
 };
