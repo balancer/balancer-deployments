186de8d7096a0ced204937d3cbc9ea0750a96e0d
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2025-08-20 09:39:15 -0300

    Deployment preparation: `ConstantPriceFeed` (#291)

--- v3/tasks/20250814-v3-weighted-pool-oracle/input.ts
+++ v3/tasks/20250814-v3-weighted-pool-oracle/input.ts
@@ -2,12 +2,14 @@ import { Task, TaskMode } from '@src';
 
 export type WeightedLPOracleDeployment = {
   Vault: string;
+  ConstantPriceFeed: string;
   FactoryVersion: string;
   OracleVersion: string;
   MockWeightedPool: string;
 };
 
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
+const ConstantPriceFeed = new Task('20250813-v3-constant-price-feed', TaskMode.READ_ONLY);
 const MockWeightedPool = new Task('20241205-v3-weighted-pool', TaskMode.READ_ONLY);
 const OracleVersion = 0;
 
@@ -15,6 +17,7 @@ const BaseVersion = { version: 0, deployment: '20250814-v3-weighted-pool-oracle'
 
 export default {
   Vault,
+  ConstantPriceFeed,
   FactoryVersion: JSON.stringify({ name: 'WeightedLPOracleFactory', ...BaseVersion }),
   OracleVersion,
   MockWeightedPool,
--- v3/tasks/20250815-v3-stable-pool-oracle/input.ts
+++ v3/tasks/20250815-v3-stable-pool-oracle/input.ts
@@ -2,12 +2,14 @@ import { Task, TaskMode } from '@src';
 
 export type StableLPOracleDeployment = {
   Vault: string;
+  ConstantPriceFeed: string;
   FactoryVersion: string;
   OracleVersion: string;
   MockStablePool: string;
 };
 
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
+const ConstantPriceFeed = new Task('20250813-v3-constant-price-feed', TaskMode.READ_ONLY);
 const MockStablePool = new Task('20250324-v3-stable-pool-v2', TaskMode.READ_ONLY);
 const OracleVersion = 0;
 
@@ -15,6 +17,7 @@ const BaseVersion = { version: 0, deployment: '20250815-v3-stable-pool-oracle' }
 
 export default {
   Vault,
+  ConstantPriceFeed,
   FactoryVersion: JSON.stringify({ name: 'StableLPOracleFactory', ...BaseVersion }),
   OracleVersion,
   MockStablePool,
