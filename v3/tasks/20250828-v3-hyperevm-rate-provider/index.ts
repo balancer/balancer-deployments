c5719339df9a6d52c980740952cbf091e460ca69
Juan Ignacio Ubeira <juani@balancer.finance>
2025-09-05 09:17:03 -0300

    Adjust deployment scripts to reduce friction in actual deployment.

--- v3/tasks/20250828-v3-hyperevm-rate-provider/index.ts
+++ v3/tasks/20250828-v3-hyperevm-rate-provider/index.ts
@@ -1,14 +1,33 @@
 import { HyperEVMRateProviderFactoryDeployment } from './input';
-import { Task, TaskRunOptions } from '@src';
+import { saveContractDeploymentTransactionHash, Task, TaskMode, TaskRunOptions } from '@src';
+import * as expectEvent from '@helpers/expectEvent';
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as HyperEVMRateProviderFactoryDeployment;
 
-  await task.deployAndVerify(
+  const factory = await task.deployAndVerify(
     'HyperEVMRateProviderFactory',
     [input.Vault, input.FactoryVersion, input.RateProviderVersion],
     from,
     force
   );
+
+  if (task.mode === TaskMode.LIVE) {
+    if (force || !task.output({ ensure: false })['MockHyperEVMRateProvider']) {
+      const receipt = await (await factory.create(input.ExampleTokenIndex, input.ExamplePairIndex)).wait();
+      const event = expectEvent.inReceipt(receipt, 'RateProviderCreated');
+      const mockRateProviderAddress = event.args.rateProvider;
+
+      saveContractDeploymentTransactionHash(mockRateProviderAddress, receipt.transactionHash, task.network);
+      task.save({ MockRateProvider: mockRateProviderAddress });
+    }
+
+    const mockRateProvider = await task.instanceAt('HyperEVMRateProvider', task.output()['MockHyperEVMRateProvider']);
+
+    await task.verify('HyperEVMRateProvider', mockRateProvider.address, [
+      input.ExampleTokenIndex,
+      input.ExamplePairIndex,
+    ]);
+  }
 };
