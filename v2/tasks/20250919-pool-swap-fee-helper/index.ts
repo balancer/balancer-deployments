beaffd288f75ced05822ddd8a6f87a6fa3eb728f
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2025-09-18 20:22:06 -0300

    Update v2/tasks/20250919-pool-swap-fee-helper/index.ts

--- v2/tasks/20250919-pool-swap-fee-helper/index.ts
+++ v2/tasks/20250919-pool-swap-fee-helper/index.ts
@@ -1,7 +1,6 @@
 import { PoolSwapFeeHelperDeployment } from './input';
 import { Task, TaskRunOptions } from '@src';
 
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as PoolSwapFeeHelperDeployment;
 
