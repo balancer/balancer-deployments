c8c1a1f6f60f45b63fa58b5f5865a0c74bca0041
Daniel <danielBeets@protonmail.com>
2025-10-29 10:46:14 +0100

    add sonic admin

--- v3/tasks/20250919-v3-pool-swap-fee-helper-v2/input.ts
+++ v3/tasks/20250919-v3-pool-swap-fee-helper-v2/input.ts
@@ -10,6 +10,7 @@ const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 // See https://github.com/BalancerMaxis/bal_addresses/blob/main/outputs/addressbook.json#L268
 const MaxiOmniMultisig = '0x9ff471F9f98F42E5151C7855fD1b5aa906b1AF7e';
 const SepoliaAdmin = '0x9098b50ee2d9E4c3C69928A691DA3b192b4C9673';
+const SonicAdmin = '0x97079F7E04B535FE7cD3f972Ce558412dFb33946';
 
 export default {
   Vault,
@@ -39,5 +40,8 @@ export default {
   },
   sepolia: {
     HelperAdmin: SepoliaAdmin,
+  },
+  sonic: {
+    HelperAdmin: SonicAdmin,
   },
 };
