a5f38967773231af2e62b5aa09ad26b8d1596951
Juan Ignacio Ubeira <juani@balancer.finance>
2026-01-23 13:01:16 -0300

    Deployment preparation: ClaimSignatureRegistry (draft).

--- v2/tasks/20260122-claim-signature-registry/test/task.fork.ts
+++ v2/tasks/20260122-claim-signature-registry/test/task.fork.ts
@@ -9,8 +9,15 @@ describeForkTest('ClaimSignatureRegistry', 'mainnet', 24291200, function () {
   let signer: SignerWithAddress;
   let claimSignatureRegistry: Contract;
 
-  const message =
-    "I confirm and agree to the following:\n- I accept the terms and conditions applicable to this claim, including all provisions of the Balancer Terms of Use and all relevant Balancer governance resolutions.\n- I acknowledge and agree that my acceptance constitutes a full and final settlement and release of any and all past, present, and future claims, liabilities, demands, actions, causes of action, damages, or losses of any kind-whether known or unknown-arising out of or related to the Balancer V2 exploit.\n- This waiver expressly includes claims against the Balancer Foundation, Balancer OpCo Limited, Balancer Onchain Limited and all affiliated entities, as well as their respective officers, directors, contributors, service providers, employees, contractors, advisors, agents, successors, and assigns (collectively, the 'Released Parties').\n- I acknowledge the limitation of liability, mandatory arbitration and all risk disclosures set forth in the Balancer Terms of Use.\n- I waive and relinquish any right to participate in any class or collective action related to the V2 exploit or the Balancer Protocol.\n- I acknowledge and agree to the SEAL Safe Harbor Agreement which was approved by governance resolution and can offer legal protection to whitehats who aid in the recovery of assets during an active exploit.- I understand that my claim will not be processed unless I accept these terms in full and without modification.";
+  const message: string =
+    'I confirm and agree to the following:\n\n' +
+    'I accept the terms and conditions applicable to this claim, including all provisions of the Balancer Terms of Use and all relevant Balancer governance resolutions.\n\n' +
+    'I acknowledge and agree that my acceptance constitutes a full and final settlement and release of any and all past, present, and future claims, liabilities, demands, actions, causes of action, damages, or losses of any kind -whether known or unknown-arising out of or related to the Balancer V2 exploit.\n\n' +
+    'This waiver expressly includes claims against the Balancer Foundation, Balancer OpCo Limited, Balancer Onchain Limited and all affiliated entities, as well as their respective officers, directors, contributors, service providers, employees, contractors, advisors, agents, successors, and assigns (collectively, the "Released Parties").\n\n' +
+    'I acknowledge the limitation of liability, mandatory arbitration and all risk disclosures set forth in the Balancer Terms of Use.\n' +
+    'I waive and relinquish any right to participate in any class or collective action related to the V2 exploit or the Balancer Protocol.\n\n' +
+    'I acknowledge and agree to the SEAL Safe Harbor Agreement which was approved by governance resolution and can offer legal protection to whitehats who aid in the recovery of assets during an active exploit.\n\n' +
+    'I understand that my claim will not be processed unless I accept these terms in full and without modification.';
 
   before('run task', async () => {
     task = new Task('20260122-claim-signature-registry', TaskMode.TEST, getForkedNetwork(hre));
