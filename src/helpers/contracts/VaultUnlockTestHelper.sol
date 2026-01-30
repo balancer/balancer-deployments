ae557c76fd7488d8188b3b5c71766065ffb0be3a
Jeff Bennett <endymionjkb@gmail.com>
2026-01-30 17:44:44 -0500

    docs: add header to VaultUnlockTestHelper

--- src/helpers/contracts/VaultUnlockTestHelper.sol
+++ src/helpers/contracts/VaultUnlockTestHelper.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity ^0.8.24;
 
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
+/**
+ * @notice Helper contract to facilitate testing calls while the Vault is unlocked.
+ * @dev See v3/tasks/20260202-v3-weighted-pool-oracle-v2/test/task.fork.ts for usage. 
+ */
 contract VaultUnlockTestHelper {
     IVault public immutable vault;
     
