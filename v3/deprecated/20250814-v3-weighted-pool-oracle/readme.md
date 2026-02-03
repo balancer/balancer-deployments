4e8ad72decf622c8063893bc7fec6e02b75e1ad7
EndymionJkb <EndymionJkb@gmail.com>
2026-02-03 13:01:39 -0500

    Deployment Preparation: Weighted Pool Oracle V2 (#369)

---
+++ v3/deprecated/20250814-v3-weighted-pool-oracle/readme.md
@@ -0,0 +1,25 @@
+# 2025-08-14 - V3 Weighted Pool Oracle
+
+> ⚠️ **DEPRECATED: do not use** ⚠️
+>
+> This version was superseded by [`WeightedLPOracle` V2](../../tasks/20260202-v3-weighted-pool-oracle-v2/), which allows creation of oracles that only work when the Balancer Vault is locked. This oracle can still be used as long as a deposit cap is implemented somewhere in the system (i.e. in the lending protocol accepting BPT as collateral); otherwise the V2 oracle is a superset of this version that allows the creator to configure an extra layer of protection against flash-minted BPT.
+
+Contains `WeightedLPOracle` and its corresponding `WeightedLPOracleFactory`.
+These contracts provide a manipulation-resistant mechanism to compute the market price of LP tokens from weighted pools given market price feeds for each of the tokens registered in the pool.
+
+## Useful Files
+
+- [Code](https://github.com/balancer/balancer-v3-monorepo/commit/1bfc266c705b59cd9c03fe1b1dec078e5f74179c).
+- [Ethereum mainnet addresses](./output/mainnet.json)
+- [Gnosis mainnet addresses](./output/gnosis.json)This oracle can still be used as long as a deposit cap is implemented somewhere in the system (i.e. in the lending protocol accepting BPT as collateral); otherwise the V2 oracle is a superset of this version that allows the creator to configure an extra layer of protection against flash-minted BPT.
+- [Arbitrum mainnet addresses](./output/arbitrum.json)
+- [Base mainnet addresses](./output/base.json)
+- [Optimism mainnet addresses](./output/optimism.json)
+- [Avalanche mainnet addresses](./output/avalanche.json)
+- [Hyperevm mainnet addresses](./output/hyperevm.json)
+- [Plasma mainnet addresses](./output/plasma.json)
+- [X-Layer mainnet addresses](./output/xlayer.json)
+- [Monad mainnet addresses](./output/monad.json)
+- [Sepolia testnet addresses](./output/sepolia.json)
+- [`WeightedLPOracle` artifact](./artifact/WeightedLPOracle.json)
+- [`WeightedLPOracleFactory` artifact](./artifact/WeightedLPOracleFactory.json)
---
+++ v3/tasks/20260202-v3-weighted-pool-oracle-v2/test/task.fork.ts
@@ -0,0 +1,96 @@
+import hre from 'hardhat';
+import { Contract } from 'ethers';
+import { deploy, describeForkTest, getForkedNetwork, Task, TaskMode } from '@src';
+import { fpMul, fromFp } from '@helpers/numbers';
+import { expect } from 'chai';
+import { ZERO_ADDRESS } from '@helpers/constants';
+import { expectRevertWithCustomError } from '@helpers/expectCustomError';
+import input from '../input';
+
+describeForkTest('WeightedLPOracle', 'base', 41509250, function () {
+  let task: Task;
+  let weightedLPOracleFactory: Contract, weightedLPOracle: Contract;
+  let poolToken: Contract;
+  let unlockHelper: Contract;
+
+  const WEIGHTED_POOL_ADDRESS = '0x4fbb7870dbe7a7ef4866a33c0eed73d395730dc0';
+
+  const ETH_FEED_ADDRESS = '0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70';
+  const USDC_FEED_ADDRESS = '0x7e860098F58bBFC8648a4311b374B1D669a2bc6B';
+
+  before('run task', async () => {
+    task = new Task('20260202-v3-weighted-pool-oracle-v2', TaskMode.TEST, getForkedNetwork(hre));
+    await task.run({ force: true });
+    weightedLPOracleFactory = await task.deployedInstance('WeightedLPOracleFactory');
+  });
+
+  before('setup contracts and addresses', async () => {
+    const vaultTask = new Task('20241204-v3-vault', TaskMode.READ_ONLY, getForkedNetwork(hre));
+    const vault = await vaultTask.deployedInstance('Vault');
+
+    poolToken = await vaultTask.instanceAt('IERC20', WEIGHTED_POOL_ADDRESS);
+
+    unlockHelper = await deploy('VaultUnlockTestHelper', [vault.address]);
+  });
+
+  it('checks version', async () => {
+    const factoryVersion = JSON.parse(await weightedLPOracleFactory.version());
+    expect(factoryVersion.deployment).to.be.eq(task.id);
+    expect(factoryVersion.name).to.be.eq('WeightedLPOracleFactory');
+    expect(factoryVersion.version).to.be.eq(input.OracleVersion);
+  });
+
+  it('creates oracle', async () => {
+    // Feeds are ordered according to the token order registered in the pool.
+    const tx = await weightedLPOracleFactory.create(
+      WEIGHTED_POOL_ADDRESS,
+      input.ShouldUseBlockTimeForOldestFeedUpdate,
+      input.ShouldRevertIfVaultUnlocked,
+      [USDC_FEED_ADDRESS, ETH_FEED_ADDRESS]
+    );
+
+    const receipt = await tx.wait();
+    const event = receipt.events?.find((e: { event: string }) => e.event === 'WeightedLPOracleCreated');
+    weightedLPOracle = await task.instanceAt('WeightedLPOracle', event?.args?.oracle);
+    expect(weightedLPOracle).to.not.be.undefined;
+    expect(weightedLPOracle).to.not.be.eq(ZERO_ADDRESS);
+
+    expect(await weightedLPOracle.getShouldUseBlockTimeForOldestFeedUpdate()).to.be.equal(
+      input.ShouldUseBlockTimeForOldestFeedUpdate
+    );
+    expect(await weightedLPOracle.getShouldRevertIfVaultUnlocked()).to.be.equal(input.ShouldRevertIfVaultUnlocked);
+  });
+
+  it('checks price feeds', async () => {
+    const priceFeeds = await weightedLPOracle.getFeeds();
+    expect(priceFeeds[0]).to.be.eq(USDC_FEED_ADDRESS);
+    expect(priceFeeds[1]).to.be.eq(ETH_FEED_ADDRESS);
+  });
+
+  it('checks prices', async () => {
+    const { prices } = await weightedLPOracle.getFeedData();
+    expect(fromFp(prices[0])).to.be.equalWithError(0.999);
+    expect(fromFp(prices[1])).to.be.equalWithError(2695);
+  });
+
+  it('gets TVL measured in USD', async () => {
+    //const { answer } = await weightedLPOracle.latestRoundData();
+    const [, answer] = await weightedLPOracle.latestRoundData();
+
+    const totalSupply = await poolToken.totalSupply();
+
+    // This is the TVL in USD at the current block.
+    expect(fromFp(fpMul(answer, totalSupply))).to.be.equalWithError(13674);
+  });
+
+  it('reverts if the Vault is unlocked', async () => {
+    expect(await weightedLPOracle.getShouldRevertIfVaultUnlocked()).to.be.true;
+
+    const callData = weightedLPOracle.interface.encodeFunctionData('latestRoundData');
+
+    await expectRevertWithCustomError(
+      unlockHelper.callWhileUnlocked(weightedLPOracle.address, callData),
+      'VaultIsUnlocked()'
+    );
+  });
+});
