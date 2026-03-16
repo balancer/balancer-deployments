6a38ba59fa1fc8d5d78e67446a79e18b7fa78eae
Gosuto Inzasheru <gosuto@maxyz.xyz>
2026-03-16 12:28:34 +0100

    Update bal_addresses GitHub URLs from BalancerMaxis to balancer org

--- v3/deprecated/20250530-v3-balancer-fee-burner/input.ts
+++ v3/deprecated/20250530-v3-balancer-fee-burner/input.ts
@@ -9,7 +9,7 @@ export type BalancerFeeBurnerDeployment = {
 const Vault = new Task('20241204-v3-vault', TaskMode.READ_ONLY);
 const ProtocolFeeSweeper = new Task('20250503-v3-protocol-fee-sweeper-v2', TaskMode.READ_ONLY);
 
-// https://github.com/BalancerMaxis/bal_addresses/blob/4b7ab007d01af33f996ae22bc70184f4eca6deeb/extras/signers.json#L72
+// https://github.com/balancer/bal_addresses/blob/4b7ab007d01af33f996ae22bc70184f4eca6deeb/extras/signers.json#L72
 const InitialOwner = '0x74E283B985EA76c55C8B48d6bD1067a418188424';
 
 export default {
