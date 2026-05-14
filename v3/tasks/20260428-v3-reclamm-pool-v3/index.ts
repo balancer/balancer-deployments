16faf6811974985176294f0deb0086fde9a56a05
Jeff Bennett <endymionjkb@gmail.com>
2026-05-13 23:10:07 -0400

    Fix parameter issue in deployment file (and add gasLimit for HyperEVM)

--- v3/tasks/20260428-v3-reclamm-pool-v3/index.ts
+++ v3/tasks/20260428-v3-reclamm-pool-v3/index.ts
@@ -77,7 +77,8 @@ export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise
           priceParams,
           newReClammPoolParams.dailyPriceShiftExponent,
           newReClammPoolParams.centerednessMargin,
-          newReClammPoolParams.salt
+          newReClammPoolParams.salt,
+          { gasLimit: 10_000_000 }
         )
       ).wait();
       const event = expectEvent.inReceipt(poolCreationReceipt, 'PoolCreated');
@@ -98,6 +99,8 @@ export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise
       initialMinPrice: newReClammPoolParams.initialMinPrice,
       initialMaxPrice: newReClammPoolParams.initialMaxPrice,
       initialTargetPrice: newReClammPoolParams.initialTargetPrice,
+      tokenAPriceIncludesRate: priceParams.tokenAPriceIncludesRate,
+      tokenBPriceIncludesRate: priceParams.tokenBPriceIncludesRate,
     };
 
     // We are now ready to verify the Pool
